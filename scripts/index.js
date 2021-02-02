263bd7f6a72c082e59887c5bc2f498e49d4ce8ac
Yulia Okhotina <y.chaser@yandex.ru>
2021-02-03 00:47:31 +0300

    Выполнена проектная работа 7

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,39 @@
+import { initialCards } from './initial-сards.js';
+import { FormValidator, validationConfig } from './formvalidator.js';
+import { Card } from './card.js';
+
+
 const editButton = document.querySelector('.button_type_edit');
 const closeButtons = document.querySelectorAll('.button_type_close');
 const addButton = document.querySelector('.button_type_add');
 const popUpEdit = document.querySelector('.popup_type_edit');
 const popUpAdd = document.querySelector('.popup_type_add');
-const popUpOpen = document.querySelector('.popup_type_image');
+export const popUpOpen = document.querySelector('.popup_type_image');
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
 const nameInput = document.querySelector('.form__item_type_name');
 const jobInput = document.querySelector('.form__item_type_job');
-const imageTitle = document.querySelector('.popup__image-title');
-const imageLink = document.querySelector('.popup__image');
+export const imageTitle = document.querySelector('.popup__image-title');
+export const imageLink = document.querySelector('.popup__image');
 const cardNameInput = document.querySelector('.form__item_type_card-name');
 const cardLinkInput = document.querySelector('.form__item_type_card-link');
 const formElement = document.querySelector('.form_type_edit');
 const newCardElement = document.querySelector('.form_type_add');
 const cardsContainerElement = document.querySelector('.gallery__list');
-const templateElement = document.querySelector('.template');
 const ESCAPE = 27;
 
+const cardValidator = new FormValidator(validationConfig, formElement);
+cardValidator.enableValidation();
+
+const newCardValidator = new FormValidator(validationConfig, newCardElement);
+newCardValidator.enableValidation();
+
 function renderList() {
-  const cardsItems = initialCards.map(composeItem);
-  cardsContainerElement.append(...cardsItems);
-}
-
-function composeItem(item) {
-  const newItem = templateElement.content.cloneNode(true);
-  const headerElement = newItem.querySelector('.gallery__item-title');
-  headerElement.textContent = item.name;
-  const imageElement = newItem.querySelector('.gallery__item-image');
-  imageElement.src = item.link;
-  imageElement.alt = item.name;
-
-  newItem.querySelector('.button_type_like').addEventListener('click', (evt) => {evt.target.classList.toggle('button_clicked');});
-
-  newItem.querySelector('.button_type_delete').addEventListener('click', (evt) => {
-    evt.target.closest('.gallery__item').remove();
-  });
-
-  imageElement.addEventListener('click', () => handlePreviewPicture(item.link, item.name));
-
-  return newItem;
+  initialCards.forEach((item) => {
+    const card = new Card(item, '.template');
+    const cardElement = card.generateCard();
+    cardsContainerElement.append(cardElement);
+});
 }
 
 function handlePreviewPicture(link, name) {
@@ -49,7 +43,7 @@ function handlePreviewPicture(link, name) {
     openPopUp(popUpOpen);
 }
 
-function openPopUp(popup) {
+export function openPopUp(popup) {
     popup.classList.add('popup_opened');
     document.addEventListener('click', closePopUpByOverlay);
     document.addEventListener('keydown', closePopUpByEsc);
@@ -82,10 +76,13 @@ function openProfileForm() {
 
 function handleCardFormSubmit(evt) {
     evt.preventDefault();
-    const newCardName = cardNameInput.value;
-    const newCardLink = cardLinkInput.value;
-    const newCard = composeItem({ name: newCardName, link: newCardLink});
-    cardsContainerElement.prepend(newCard);
+    const item = {
+      name: cardNameInput.value,
+      link: cardLinkInput.value
+    }
+    const newCard = new Card(item, '.template');
+    const newCardElement = newCard.generateCard();
+    cardsContainerElement.prepend(newCardElement);
     closePopUp(popUpAdd);
 }
 
